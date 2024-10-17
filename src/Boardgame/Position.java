29531c2ed339f79880770bbc62bf0796a1242eaa
DallaProRacing <103423@aluno.uricer.edu.br>
2024-10-17 15:37:57 -0300

    Criando classe Position

---
+++ src/Boardgame/Position.java
@@ -0,0 +1,29 @@
+package Boardgame;
+
+public class Position {
+
+		private int row;
+		private int column;
+		
+		public int getRow() {
+			return row;
+		}
+		public void setRow(int row) {
+			this.row = row;
+		}
+		public int getColunm() {
+			return column;
+		}
+		public void setColunm(int colunm) {
+			this.column = colunm;
+		}
+		public Position(int row, int colunm) {
+			this.row = row;
+			this.column = colunm;
+		}
+		
+		@Override
+		public String toString() {
+			return row + ", " + column;
+		}
+}
